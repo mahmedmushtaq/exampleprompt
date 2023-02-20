472dfa3567ea8c940d8ef936b90b0a5c03aa3195
mahmedmushtaq <mahmedmushtaq296@gmail.com>
2023-02-20 23:16:42 +0500

    updated favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap"
-        data-tag="font"
-      />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          data-tag="font"
+        />
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
